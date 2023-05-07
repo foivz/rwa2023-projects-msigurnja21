62c715ee5c3811eafd6f0f8e2e6258dc0913fec1
msigurnja21 <126813912+msigurnja21@users.noreply.github.com>
2023-05-07 15:36:15 +0200

    Add files via upload

---
+++ Software/FrmPrijava.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alat_za_praćenje_osobnih_financija
+{
+    public partial class FrmPrijava : Form
+    {
+        public FrmPrijava()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void btnPrijava_Click(object sender, EventArgs e)  // Prijava u sustav 
+        {
+            if(txtKorisnickoIme.Text == "test" && txtLozinka.Text == "test")    //ako su uneseni dobri korisnički podaci
+            {
+                FrmTroškovi frmtroškovi = new FrmTroškovi();            //stvaranje objekta nove forme za prikaz troškova
+                Hide();                                                 //skrivanje forme za prijavu
+                frmtroškovi.ShowDialog();                               //otvaranje forme troškova    
+                Close();                                                //zatvaranje forme za prijavu
+            }
+            else                                                                        //ako su uneseni pogrešni korisnički podaci
+            {
+                MessageBox.Show("Pogrešni korisnički podaci", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Software/FrmUnesiTrošak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alat_za_praćenje_osobnih_financija
+{
+    public partial class FrmUnesiTrošak : Form
+    {
+        public FrmUnesiTrošak()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodajŠifru_Click(object sender, EventArgs e)
+        {
+            FrmNovaŠifra frmNovaŠifra = new FrmNovaŠifra();  //stvaranje objekta nove forme za unos nove šifre kategorije
+            frmNovaŠifra.ShowDialog();                       //otvaranje forme za unos nove šifre kategorije
+        }
+    }
+}
